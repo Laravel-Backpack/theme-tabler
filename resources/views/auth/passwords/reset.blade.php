c0a2b3551d7a68620fb9e8a5c7bddc84a696c96e
Mauro Martinez <maurohmartinez@gmail.com>
2023-04-12 22:00:56 +0300

    Register, forgot password pages

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,83 +1,78 @@
-@extends(backpack_view('layouts.plain'))
+@extends(backpack_view('layouts.auth'))
 
 @section('content')
-    <div class="row justify-content-center align-items-center d-flex flex-row min-vh-100">
-        <div class="col-12 col-md-9 col-lg-6">
-            <h3 class="text-center mb-4">{{ trans('backpack::base.reset_password') }}</h3>
-            <div class="nav-steps-wrapper">
-                <ul class="nav nav-tabs">
-                      <li class="nav-item"><a class="nav-link disabled text-muted"><strong>{{ trans('backpack::base.step') }} 1.</strong> {{ trans('backpack::base.confirm_email') }}</a></li>
-                      <li class="nav-item active"><a class="nav-link active"><strong>{{ trans('backpack::base.step') }} 2.</strong> {{ trans('backpack::base.choose_new_password') }}</a></li>
-                </ul>
+    <div class="page page-center">
+        <div class="container container-tight py-4">
+            <div class="text-center mb-4 display-6">
+                {!! backpack_theme_config('project_logo') !!}
             </div>
-            <div class="nav-tabs-custom">
-                <div class="tab-content border bg-white">
-                  <div class="tab-pane active p-3" id="tab_1">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <form class="col-md-12 p-t-10" role="form" method="POST" action="{{ route('backpack.auth.password.reset') }}">
-                        {!! csrf_field() !!}
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group mb-3"">
-                            <label class="control-label" for="email">{{ trans('backpack::base.email_address') }}</label>
-
-                            <div>
-                                <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" id="email" value="{{ $email ?? old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group mb-3"">
-                            <label class="control-label" for="password">{{ trans('backpack::base.new_password') }}</label>
-
-                            <div>
-                                <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group mb-3"">
-                            <label class="control-label" for="password_confirmation">{{ trans('backpack::base.confirm_new_password') }}</label>
-                            <div>
-                                <input type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" name="password_confirmation" id="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group mb-3">
-                            <div>
-                                <button type="submit" class="btn btn-block btn-primary">
-                                    {{ trans('backpack::base.change_password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                    <div class="clearfix"></div>
-                  </div>
-                  {{-- /.tab-pane --}}
+            <div class="card">
+                <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs" data-bs-toggle="tabs" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <div class="nav-link disabled" data-bs-toggle="tab" aria-selected="true" role="tab">{{ trans('backpack::base.step') }} 1</div>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <div class="nav-link active" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">{{ trans('backpack::base.step') }} 2 — {{ trans('backpack::base.choose_new_password') }}</div>
+                        </li>
+                        <li class="nav-item ms-auto" role="presentation">
+                            <a href="javascript:void(0);" onclick="colorMode.switch()" class="nav-link hide-theme-dark" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable dark mode" data-bs-original-title="Enable dark mode">
+                                <i class="la la-sun fs-2"></i>
+                            </a>
+                            <a href="javascript:void(0);" onclick="colorMode.switch()" class="nav-link hide-theme-light" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable light mode" data-bs-original-title="Enable light mode">
+                                <i class="la la-moon fs-2"></i>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                {{-- /.tab-content --}}
-              </div>
+                <div class="card-body">
+                    <h3 class="mb-3">{{ trans('backpack::base.reset_password') }}</h3>
+                    <div class="tab-content">
+                        <div class="tab-pane active show" id="tabs-home-7" role="tabpanel">
+                            @if (session('status'))
+                                <div class="alert alert-success mt-3">
+                                    {{ session('status') }}
+                                </div>
+                            @else
+                                <form class="col-md-12 p-t-10" role="form" method="POST" action="{{ route('backpack.auth.password.reset') }}">
+                                    @csrf
+                                    <input type="hidden" name="token" value="{{ $token }}">
+
+                                    <div class="mb-3">
+                                        <label class="form-label" for="email">{{ trans('backpack::base.email_address') }}</label>
+                                        <input autofocus type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" id="email" value="{{ old('email') }}">
+                                        @if ($errors->has('email'))
+                                            <div class="invalid-feedback">{{ $errors->first('email') }}</div>
+                                        @endif
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label class="form-label" for="password">{{ trans('backpack::base.password') }}</label>
+                                        <input type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" id="password" value="">
+                                        @if ($errors->has('password'))
+                                            <div class="invalid-feedback">{{ $errors->first('password') }}</div>
+                                        @endif
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label class="form-label" for="password_confirmation">{{ trans('backpack::base.confirm_password') }}</label>
+                                        <input type="password" class="form-control {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}" name="password_confirmation" id="password_confirmation" value="">
+                                        @if ($errors->has('password_confirmation'))
+                                            <div class="invalid-feedback">{{ $errors->first('password_confirmation') }}</div>
+                                        @endif
+                                    </div>
+
+                                    <div class="form-footer">
+                                        <button type="submit" class="btn btn-primary w-100">
+                                            {{ trans('backpack::base.change_password') }}
+                                        </button>
+                                    </div>
+                                </form>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,73 +1,59 @@
-@extends(backpack_view('layouts.plain'))
+@extends(backpack_view('layouts.auth'))
 
 @section('content')
-    <div class="row justify-content-center align-items-center d-flex flex-row min-vh-100">
-        <div class="col-12 col-md-8 col-lg-6">
-            <h3 class="text-center mb-4">{{ trans('backpack::base.register') }}</h3>
+    <div class="page page-center">
+        <div class="container container-tight py-4">
+            <div class="text-center mb-4 display-6">
+                {!! backpack_theme_config('project_logo') !!}
+            </div>
             <div class="card p-4 mb-0">
-                <div class="card-body">
-                    <form class="col-md-12 p-t-10" role="form" method="POST" action="{{ route('backpack.auth.register') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group mb-2">
-                            <label class="control-label" for="name">{{ trans('backpack::base.name') }}</label>
-
-                            <div>
-                                <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" id="name" value="{{ old('name') }}">
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                <div class="d-flex justify-content-end w-100">
+                    <a href="javascript:void(0);" onclick="colorMode.switch()" class="nav-link px-0 hide-theme-dark" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable dark mode" data-bs-original-title="Enable dark mode">
+                        <i class="la la-sun fs-2"></i>
+                    </a>
+                    <a href="javascript:void(0);" onclick="colorMode.switch()" class="nav-link px-0 hide-theme-light" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable light mode" data-bs-original-title="Enable light mode">
+                        <i class="la la-moon fs-2"></i>
+                    </a>
+                </div>
+                <div class="card-body pt-0">
+                    <h2 class="card-title text-center mb-4">{{ trans('backpack::base.register') }}</h2>
+                    <form role="form" method="POST" action="{{ route('backpack.auth.register') }}">
+                        @csrf
+                        <div class="mb-3">
+                            <label class="form-label" for="name">{{ trans('backpack::base.name') }}</label>
+                            <input autofocus type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" name="name" id="name" value="{{ old('name') }}">
+                            @if ($errors->has('name'))
+                                <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+                            @endif
                         </div>
 
-                        <div class="form-group mb-2">
-                            <label class="control-label" for="{{ backpack_authentication_column() }}">{{ config('backpack.base.authentication_column_name') }}</label>
-
-                            <div>
-                                <input type="{{ backpack_authentication_column()==backpack_email_column()?'email':'text'}}" class="form-control{{ $errors->has(backpack_authentication_column()) ? ' is-invalid' : '' }}" name="{{ backpack_authentication_column() }}" id="{{ backpack_authentication_column() }}" value="{{ old(backpack_authentication_column()) }}">
-
-                                @if ($errors->has(backpack_authentication_column()))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first(backpack_authentication_column()) }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="{{ backpack_authentication_column() }}">{{ config('backpack.base.authentication_column_name') }}</label>
+                            <input type="{{ backpack_authentication_column()==backpack_email_column()?'email':'text'}}" class="form-control {{ $errors->has(backpack_authentication_column()) ? 'is-invalid' : '' }}" name="{{ backpack_authentication_column() }}" id="{{ backpack_authentication_column() }}" value="{{ old(backpack_authentication_column()) }}">
+                            @if ($errors->has(backpack_authentication_column()))
+                                <div class="invalid-feedback">{{ $errors->first(backpack_authentication_column()) }}</div>
+                            @endif
                         </div>
 
-                        <div class="form-group mb-2">
-                            <label class="control-label" for="password">{{ trans('backpack::base.password') }}</label>
-
-                            <div>
-                                <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="password">{{ trans('backpack::base.password') }}</label>
+                            <input type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" id="password" value="">
+                            @if ($errors->has('password'))
+                                <div class="invalid-feedback">{{ $errors->first('password') }}</div>
+                            @endif
                         </div>
 
-                        <div class="form-group mb-2">
-                            <label class="control-label" for="password_confirmation">{{ trans('backpack::base.confirm_password') }}</label>
-
-                            <div>
-                                <input type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" name="password_confirmation" id="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="password_confirmation">{{ trans('backpack::base.confirm_password') }}</label>
+                            <input type="password" class="form-control {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}" name="password_confirmation" id="password_confirmation" value="">
+                            @if ($errors->has('password_confirmation'))
+                                <div class="invalid-feedback">{{ $errors->first('password_confirmation') }}</div>
+                            @endif
                         </div>
 
                         <div class="form-group mt-3">
                             <div>
-                                <button type="submit" class="btn btn-block btn-primary">
+                                <button type="submit" class="btn btn-primary w-100">
                                     {{ trans('backpack::base.register') }}
                                 </button>
                             </div>
