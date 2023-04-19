1a6b5bdf3c5bf3bfcb0ba72c7ae92d61e2e2626d
Mauro Martinez <maurohmartinez@gmail.com>
2023-04-19 11:09:59 +0300

    Sticky header to false by default

--- config/theme-tabler.php
+++ config/theme-tabler.php
@@ -61,7 +61,7 @@ return [
          * Fix the top-header component (present in all layout types) and the menu when the layout type is set as "horizontal".
          * This value is skipped when the layout type is horizontal-overlap, using false as default.
          */
-        'useStickyHeader' => true,
+        'useStickyHeader' => false,
 
         /**
          * When true, the content area will take the whole screen width.
