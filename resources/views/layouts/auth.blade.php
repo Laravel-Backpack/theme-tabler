509b544be96d9f9b1432cf5e9a437b28404a8bd7
Mauro Martinez <maurohmartinez@gmail.com>
2023-04-24 11:16:45 +0300

    Move Switch Mode to top corner

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -12,12 +12,29 @@
             background-color: transparent !important;
             border: none !important;
         }
+        .switch-mode {
+            position: absolute;
+            top: 0;
+            right: 0;
+            z-index: 999;
+        }
     </style>
 </head>
 
 <body class="o-auto {{ backpack_theme_config('classes.body') }} @if(backpack_theme_config('auth_layout') === 'cover') d-flex flex-column theme-light @endif">
 
 @include(backpack_view('layouts.partials.light-dark-mode'))
+
+@if(backpack_theme_config('options.showColorModeSwitcher'))
+    <div class="switch-mode p-3">
+        <a href="javascript:void(0);" onclick="colorMode.switch()" class="hide-theme-dark" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable dark mode" data-bs-original-title="Enable dark mode">
+            <i class="la la-sun fs-2"></i>
+        </a>
+        <a href="javascript:void(0);" onclick="colorMode.switch()" class="hide-theme-light" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable light mode" data-bs-original-title="Enable light mode">
+            <i class="la la-moon fs-2"></i>
+        </a>
+    </div>
+@endif
 
 @yield('content')
 
