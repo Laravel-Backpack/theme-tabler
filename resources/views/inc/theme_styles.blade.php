93cd6f7b36baccdfeff6fbb8dbc3ee8e12f0a711
Mauro Martinez <maurohmartinez@gmail.com>
2023-04-26 16:26:15 +0300

    Rollback on backpack colors file - part 2

--- resources/views/inc/theme_styles.blade.php
+++ resources/views/inc/theme_styles.blade.php
@@ -1,15 +1,16 @@
 @basset('https://unpkg.com/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css')
 
-@if(backpack_theme_config('options.overrideColorSkin'))
-    @basset(base_path('vendor/backpack/theme-tabler/resources/assets/css/colors.css'))
-@endif
-
 @basset(base_path('vendor/backpack/crud/src/resources/assets/css/common.css'))
 @basset(base_path('vendor/backpack/theme-tabler/resources/assets/css/tabler.css'))
 
 @basset('https://unpkg.com/animate.css@4.1.1/animate.compat.css')
 @basset('https://unpkg.com/noty@3.2.0-beta-deprecated/lib/noty.css')
 @basset('https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css')
+
+@if(backpack_theme_config('options.overrideColorSkin'))
+    @basset(base_path('vendor/backpack/theme-tabler/resources/assets/css/colors.css'))
+    @basset(base_path('vendor/backpack/theme-tabler/resources/assets/css/skin.css'))
+@endif
 
 <link rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap"
