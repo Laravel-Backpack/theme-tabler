30e40daacbfca4af5e77cfc891fa87064a6e508a
Pedro X <pxpm88@gmail.com>
2023-06-12 10:54:33 +0100

    fix config at runtime

--- src/AddonServiceProvider.php
+++ src/AddonServiceProvider.php
@@ -9,7 +9,19 @@ class AddonServiceProvider extends ServiceProvider
     use AutomaticServiceProvider;
 
     protected $vendorName = 'backpack';
+
     protected $packageName = 'theme-tabler';
+
     protected $commands = [];
+
     protected $theme = true;
+
+    public function boot()
+    {
+        $this->autoboot();
+
+        $overlapEffectUrls = array_map(fn ($item) => backpack_url($item), config('backpack.theme-tabler.options.urlsUsingOverLapEffect', []));
+        
+        app('config')->set('backpack.theme-tabler.options.urlsUsingOverLapEffect', $overlapEffectUrls);
+    }
 }
