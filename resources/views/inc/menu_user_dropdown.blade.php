f0d36166690cf8238565b5f45760b9933ba6d877
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-06-29 19:52:25 +0300

    eliminate hardcoded gravatar getting

--- resources/views/inc/menu_user_dropdown.blade.php
+++ resources/views/inc/menu_user_dropdown.blade.php
@@ -1,6 +1,6 @@
 <div class="nav-item dropdown">
     <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-        <span class="avatar avatar-sm" style="background-image: url({{ app(Creativeorange\Gravatar\Gravatar::class)->get(backpack_user()->email, ['size' => 80]) }})"></span>
+        <span class="avatar avatar-sm" style="background-image: url({{ backpack_avatar_url(backpack_auth()->user()) }})"></span>
         <div class="d-none d-xl-block ps-2">
             <div>{{ backpack_user()->name }}</div>
             <div class="mt-1 small text-muted">{{ __('Admin') }}</div>
