9121edac64a8fcc734effd0aea6ff0eb0141a361
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2023-06-29 12:56:03 +0300

    fix horizontal_overlap layout not showing overlap after merge conflicts

--- resources/views/layouts/_horizontal_overlap/menu_container.blade.php
+++ resources/views/layouts/_horizontal_overlap/menu_container.blade.php
@@ -1,4 +1,4 @@
 @extends(backpack_view('layouts._horizontal.menu_container'), [
-    'theme' => 'dark', 
-    'overlap' => in_array(url()->current(), backpack_theme_config('options.urlsUsingOverLapEffect') ?? []),
+    'theme' => 'dark',
+    'overlap' => true,
 ])
