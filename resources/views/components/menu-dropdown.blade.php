24cd70ad033b3287a7d71078d5dc21c06d466967
Pedro X <pxpm88@gmail.com>
2023-07-04 16:46:48 +0100

    add open option to menu dropdowns

--- resources/views/components/menu-dropdown.blade.php
+++ resources/views/components/menu-dropdown.blade.php
@@ -9,7 +9,7 @@
         @if($icon)<i class="nav-icon {{ $icon }} d-block d-lg-none d-xl-block"></i>@endif
         @if($title) <span>{{ $title }}</span>@endif
     </a>
-    <div class="dropdown-menu" data-bs-popper="static">
+    <div class="dropdown-menu {{ $open ? 'show' : '' }}" data-bs-popper="static">
     {!! $slot !!}
     </div>
 </li>
