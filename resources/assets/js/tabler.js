d2ef08038c1d0afd4942e980a222c6b754b376c2
Karan Datwani <karandatwani92@gmail.com>
2023-07-15 14:34:59 +0530

    init

--- resources/assets/js/tabler.js
+++ resources/assets/js/tabler.js
@@ -4,7 +4,7 @@ document.querySelectorAll('aside a.nav-link.dropdown-toggle').forEach(el => el.d
 
 // Bar layout - Highlight active section/page
 [...document.querySelectorAll('aside a[href], header.top a[href]')]
-    .filter(el => window.location.href.startsWith(el.href) && !el.href.endsWith('#'))
+    .filter(el => window.location.href.split("#")[0].split("?")[0] === el.href)
     .forEach(el => {
         while (/(nav-item|nav-link|dropdown)/.test(el.className)) {
             if (el.previousElementSibling?.dataset.bsAutoClose === 'false') el.classList.add('show');
