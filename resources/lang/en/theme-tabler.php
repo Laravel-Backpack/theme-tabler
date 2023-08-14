4f62c99d1867126c4d55c9224210d0e4eb94fde1
Antonio Almeida <promatik@gmail.com>
2023-08-14 08:12:29 +0100

    Added translations Keep consistent validations on menu dropdown component

---
+++ resources/lang/en/theme-tabler.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // error page
+    'error-title' => 'Error :error',
+    'error-button' => 'Take me home',
+
+    // color mode
+    'color-mode-description' => 'Using :mode color mode',
+    'color-mode-system' => 'system',
+    'color-mode-light' => 'light',
+    'color-mode-dark' => 'dark',
+];
---
+++ resources/lang/pt/theme-tabler.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // error page
+    'error-title' => 'Erro :error',
+    'error-button' => 'Voltar ao início',
+
+    // color mode
+    'color-mode-description' => 'A utilizar o modo :mode',
+    'color-mode-system' => 'predefinido do sistema',
+    'color-mode-light' => 'claro',
+    'color-mode-dark' => 'escuro',
+];
