ac3c82a8e96cf8a7aee8438e83870f061ca27a6b
Antonio Almeida <promatik@gmail.com>
2023-08-15 17:40:21 +0100

    Moved error translations to crud

--- resources/lang/en/theme-tabler.php
+++ resources/lang/en/theme-tabler.php
@@ -1,10 +1,6 @@
 <?php
 
 return [
-    // error page
-    'error-title' => 'Error :error',
-    'error-button' => 'Take me home',
-
     // color mode
     'color-mode-description' => 'Using :mode color mode',
     'color-mode-system' => 'system',
--- resources/lang/pt/theme-tabler.php
+++ resources/lang/pt/theme-tabler.php
@@ -1,10 +1,6 @@
 <?php
 
 return [
-    // error page
-    'error-title' => 'Erro :error',
-    'error-button' => 'Voltar ao início',
-
     // color mode
     'color-mode-description' => 'A utilizar o modo :mode',
     'color-mode-system' => 'predefinido do sistema',
