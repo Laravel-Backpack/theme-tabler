ad7f73b4daa413d8600cae8ea00a1227913d307a
Pedro X <pxpm88@gmail.com>
2023-08-15 14:09:17 +0100

    fix widgets

--- resources/views/widgets/progress_white.blade.php
+++ resources/views/widgets/progress_white.blade.php
@@ -13,30 +13,32 @@
     @endif
     <div class="card-body">
       @if (isset($widget['value']))
-      <div class="text-value"><strong>{!! $widget['value'] !!}</strong></div>
+        <div class="h1 mb-3 @if(($widget['ribbon'][0] ?? '') === 'bottom') pe-3 @endif">{!! $widget['value'] !!}</div>
       @endif
 
       @if (isset($widget['description']))
-      <div>{!! $widget['description'] !!}</div>
+        <div class="card-text @if(($widget['ribbon'][0] ?? '') === 'bottom') pe-3 @endif">{!! $widget['description'] !!}</div>
       @endif
 
       @if (isset($widget['progress']))
-      <div class="progress progress-xs my-2">
-        <div class="{{ $widget['progressClass'] ?? 'progress-bar' }} bg-{{$accentColor}}" style="width: {{ $widget['progress']  }}%" role="progressbar" aria-valuenow="{{ $widget['progress']  }}" aria-valuemin="0" aria-valuemax="100" aria-label="{{ $widget['progress']  }}% Complete">
-            <span class="visually-hidden">{{ $widget['progress']  }}% Complete</span>
+        <div class="progress progress-xs my-2">
+          <div class="{{ $widget['progressClass'] ?? 'progress-bar' }} bg-{{$accentColor}}" style="width: {{ $widget['progress']  }}%" role="progressbar" aria-valuenow="{{ $widget['progress']  }}" aria-valuemin="0" aria-valuemax="100" aria-label="{{ $widget['progress']  }}% Complete">
+              <span class="visually-hidden">{{ $widget['progress']  }}% Complete</span>
+          </div>
         </div>
-    </div>
       @endif
 
       @if (isset($widget['hint']))
-      <small>{!! $widget['hint'] !!}</small>
+        <div class="d-flex mb-2">
+            <div class="subheader @if(($widget['ribbon'][0] ?? '') === 'bottom') pe-3 @endif">{!! $widget['hint'] !!}</div>
+        </div>
       @endif
     </div>
 
     @if (isset($widget['footer_link']))
-    <div class="card-footer px-3 py-2">
-      <a class="btn-block text-muted d-flex justify-content-between align-items-center" href="{{ $widget['footer_link'] ?? '#' }}"><span class="small font-weight-bold">{{ $widget['footer_text'] ?? 'View more' }}</span><i class="la la-angle-right"></i></a>
-    </div>
+      <div class="card-footer px-3 py-2">
+        <a class="btn-block text-muted d-flex justify-content-between align-items-center" href="{{ $widget['footer_link'] ?? '#' }}"><span class="small font-weight-bold">{{ $widget['footer_text'] ?? 'View more' }}</span><i class="la la-angle-right"></i></a>
+      </div>
     @endif
   </div>
 @includeWhen(!empty($widget['wrapper']), backpack_view('widgets.inc.wrapper_end'))
