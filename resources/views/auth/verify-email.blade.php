3b3ad2d6518160b6189c3166903df706a98bff72
Cristian Tăbăcitu <cristian.tabacitu@digitallyhappy.com>
2023-08-30 13:02:17 +0300

    Apply suggestions from code review

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -11,8 +11,8 @@
                 {{ trans('backpack::base.verify_email.email_verification_required') }}
 
                 @if (session('status') == 'verification-link-sent')
-                    <div class="mb-4 mt-2">
-                    <div class="alert alert-important alert-success" role="alert">
+                    <div class="mt-4">
+                    <div class="alert alert-important alert-success my-0" role="alert">
                       <div class="d-flex">
                         <div>
                           <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l5 5l10 -10"></path></svg>
