bc49799fdd9f656ec1f03a98e6ed5bcb969f16eb
Antonio Almeida <promatik@gmail.com>
2023-09-12 12:57:18 +0100

    Replaced translation keys for error page

--- resources/views/errors/blank.blade.php
+++ resources/views/errors/blank.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ trans('backpack::base.error.title', ['error' => $error_number]) }}</title>
+        <title>{{ trans('backpack::base.error_page.title', ['error' => $error_number]) }}</title>
 
         @include(backpack_view('inc.theme_styles'))
         @include(backpack_view('inc.styles'))
--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -11,7 +11,7 @@
     <div class="empty-action">
       <a href="./." class="btn btn-primary">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l14 0"></path><path d="M5 12l6 6"></path><path d="M5 12l6 -6"></path></svg>
-        {{ trans('backpack::base.error.button') }}
+        {{ trans('backpack::base.error_page.button') }}
       </a>
     </div>
   </div>
