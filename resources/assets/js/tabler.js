4dbcd2f06f4e7bced45be3fe748d45c916037d62
Pedro X. <pxpm88@gmail.com>
2024-02-22 14:20:24 +0000

    fix nested menus in tabler

--- resources/assets/js/tabler.js
+++ resources/assets/js/tabler.js
@@ -1,51 +1,18 @@
-// Set menu auto close state if not set by developer
-document.querySelectorAll('header a.nav-link.dropdown-toggle').forEach(function(el) {
-    if (!el.getAttribute("data-bs-auto-close")) {
-        el.dataset.bsAutoClose = 'true';
-    }
-});
-document.querySelectorAll('aside a.nav-link.dropdown-toggle').forEach(function(el) {
-    if (!el.getAttribute("data-bs-auto-close")) {
-        el.dataset.bsAutoClose = 'false';
-    }
-});
-
 // Bar layout - Highlight active section/page
 [...document.querySelectorAll('aside a[href], header.top a[href]')]
     .filter(el => window.location.href.split("#")[0].split("?")[0] === el.href)
     .forEach(el => {
         while (/(nav-item|nav-link|dropdown)/.test(el.className)) {
-            if (el.previousElementSibling?.dataset.bsAutoClose === 'false')  {
-                el.classList.add('show');
-            }
-            let container = el.parentElement?.parentElement;
-            let parentContainer = container;
+            let parentContainer = el.parentElement?.parentElement;
             while (parentContainer.classList.contains('dropend')) {
                 parentContainer.querySelector('a.dropdown-toggle').classList.add('active');
                 parentContainer = parentContainer.parentElement?.parentElement;
             }
+           
             if (parentContainer.parentElement.parentElement.parentElement.parentElement.tagName.toLowerCase() === 'aside') {
-                parentContainer.querySelector('a.dropdown-toggle').classList.add('show');
-                parentContainer.querySelector('div.dropdown-menu').classList.add('show');
-                [...parentContainer.querySelectorAll('.active')].forEach(function(activeDropdown) {
-                    if(activeDropdown.classList.contains('dropdown-toggle') && activeDropdown !== el) {
-                        activeDropdown.classList.add('show');
-                        activeDropdown.nextElementSibling.classList.add('show');
-                    }
-                });
+                parentContainer.querySelector('a.dropdown-toggle').classList.add('active');
             } else if (parentContainer.classList.contains('nav-item')) {
                 parentContainer.classList.add('active');
-            
-                parentContainer.querySelector('a.dropdown-toggle').addEventListener('click', function () {
-                    [...parentContainer.querySelectorAll('.active')].forEach(function(activeDropdown) {
-                        if (activeDropdown.classList.contains('dropdown-toggle') && activeDropdown !== el) {
-                           setTimeout(function() {
-                            activeDropdown.click();               
-                            }, 10);
-                            
-                        }
-                    });
-                }); 
             }
             el.classList.add('active');
             el = el.parentElement;
--- resources/views/components/menu-dropdown.blade.php
+++ resources/views/components/menu-dropdown.blade.php
@@ -1,5 +1,8 @@
+@php
+    $dropdownId = 'dropdown-' . Str::random(8);
+@endphp
 @if($nested)
-    <div class="dropend">
+<div class="dropend">
         <a {{ $attributes->merge([
             'class' => 'dropdown-item dropdown-toggle',
             'href' => $link ?? '#',
@@ -7,28 +10,15 @@
             'role' => 'button',
             'aria-expanded' => 'false',
             'data-bs-auto-close' => 'outside',
+            'id' => $dropdownId, 
             ]) }}>
             @if($icon)<i class="nav-icon {{ $icon }} d-block d-lg-none d-xl-block"></i>@endif
             @if($title)<span>{{ $title }}</span>@endif
         </a>
-        <div class="dropdown-menu {{ $open ? 'show' : '' }}">
+        <div class="dropdown-menu {{ $open ? 'show' : '' }} dropdown-submenu" aria-labelledby="{{$dropdownId}}">
             {!! $slot !!}
         </div>
-    </div>
-
-    @bassetBlock('nested-dropdown-menu.js')
-    <script type="text/javascript">
-        document.querySelectorAll('aside a.nav-link.dropdown-toggle').forEach(function(el) {
-            el.dataset.bsAutoClose = 'false';
-        });
-        document.querySelectorAll('header a.nav-link.dropdown-toggle').forEach(function(el) {
-            let dropdownMenu = el.nextElementSibling;
-            let hasNested = dropdownMenu.querySelector('.dropend');
-
-            el.dataset.bsAutoClose = hasNested ? 'outside' : 'true';
-        });
-    </script>
-    @endBassetBlock
+</div>
 @else
 <li class="nav-item dropdown">
     <a {{ $attributes->merge([
@@ -37,13 +27,13 @@
         'data-bs-toggle'=> 'dropdown',
         'role' => 'button',
         'aria-expanded' => 'false',
-        'data-bs-auto-close' => 'true',
+        'data-bs-auto-close' => 'outside',
     ]) }}>
 
         @if($icon)<i class="nav-icon {{ $icon }} d-block d-lg-none d-xl-block"></i>@endif
         @if($title)<span>{{ $title }}</span>@endif
     </a>
-    <div class="dropdown-menu {{ $open ? 'show' : '' }}" data-bs-popper="static">
+    <div class="dropdown-menu {{ $open ? 'show' : '' }}" data-bs-popper="static" aria-labelledby="{{$dropdownId}}">
     {!! $slot !!}
     </div>
 </li>
