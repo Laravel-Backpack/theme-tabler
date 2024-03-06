993c5cc61adc27991db5d70fd58a49d4c1298168
Pedro X. <pxpm88@gmail.com>
2024-03-06 11:47:25 +0000

    cleanup and remove media query

--- resources/views/inc/sidebar_content.blade.php
+++ resources/views/inc/sidebar_content.blade.php
@@ -4,24 +4,15 @@
 @bassetBlock('tabler-menu-javascript.js')
 <script type="text/javascript">
     document.addEventListener("DOMContentLoaded", function() {
-        [...document.querySelectorAll('aside .dropdown-toggle.active')].forEach(el => {
+        [...document.querySelectorAll('aside .dropdown-toggle.active, header.top .dropdown-toggle.active')].forEach(el => {
             let bsDropdown = bootstrap.Dropdown.getInstance(el);
             if(typeof bsDropdown !== 'undefined' && bsDropdown !== null) {
                 bsDropdown.show();
             }
-            
-            el.blur();
-
-            
+            el.blur();            
         });
-        [...document.querySelectorAll('header.top .dropdown-toggle.active')].forEach(el => {
-            let bsdropdown = bootstrap.Dropdown.getInstance(el);
-            bsdropdown.show();
-            el.blur(); 
-        }); 
 
         [...document.querySelectorAll('header.top .nav-item.dropdown')].forEach(el => {
-            console.log(el);
             let bsDropdown = bootstrap.Dropdown.getInstance(el.firstElementChild);
             if(typeof bsDropdown !== 'undefined' && bsDropdown !== null) {
                 bsDropdown._config.autoClose = 'outside';
