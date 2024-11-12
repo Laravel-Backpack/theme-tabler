9830b0b40a68d5c1b8885fd81b72d53efe2bf2cf
pxpm <pxpm88@gmail.com>
2024-11-12 16:18:17 +0000

    wip

--- src/AddonServiceProvider.php
+++ src/AddonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Backpack\ThemeTabler;
 
+use Backpack\Basset\Facades\Basset;
 use Backpack\CRUD\ThemeServiceProvider;
 use Illuminate\View\Compilers\BladeCompiler;
 use Illuminate\Support\Facades\Blade;
@@ -19,5 +20,7 @@ class AddonServiceProvider extends ThemeServiceProvider
             Blade::componentNamespace('Backpack\\ThemeTabler\\View\\Components', $this->packageName);
         });
         
+        Basset::map('bp-tabler-css', 'https://unpkg.com/@tabler/core@1.0.0-beta19/dist/css/tabler.min.css', ['integrity' => 'sha384-wIPtnwKyfjylOkP4ccy+aMlHOLIfVWHDdlJ35pBwbLDH4HHXq5yG4NknAhu6Rraf', 'crossorigin' => 'anonymous']);
+        Basset::map('bp-tabler-js', 'https://unpkg.com/@tabler/core@1.0.0-beta19/dist/js/tabler.min.js', ['integrity' => 'sha384-DnBu3uHMkeDdH3JZaljVS533JqQjgcRVO+uZ3YiBXQYcYITM6HER+4+RPGobVEEZ', 'crossorigin' => 'anonymous']);
     }
 }
