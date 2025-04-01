4e1d878e1e9ad9b06ac123c28c007e16bed4625a
pxpm <pxpm88@gmail.com>
2025-04-01 19:12:45 +0100

    wip

--- resources/views/inc/theme_scripts.blade.php
+++ resources/views/inc/theme_scripts.blade.php
@@ -1,2 +1,2 @@
 @basset(base_path('vendor/backpack/theme-tabler/resources/assets/js/tabler.js'))
-@basset('https://unpkg.com/@tabler/core@1.0.0-beta19/dist/js/tabler.min.js')
+@basset('https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta19/dist/js/tabler.min.js')
