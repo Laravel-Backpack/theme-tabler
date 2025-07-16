975cf9ba18cf2c8579714d3d898dd82c33cfe8d2
pxpm <pxpm88@gmail.com>
2025-07-16 14:10:30 +0100

    update assets

--- resources/views/inc/theme_scripts.blade.php
+++ resources/views/inc/theme_scripts.blade.php
@@ -1,2 +1,2 @@
-@basset('bp-tabler-js')
+@basset('https://cdn.jsdelivr.net/npm/@tabler/core@1.3.2/dist/js/tabler.min.js', true, ['integrity' => 'sha384-DaKT+KCi9ypTlo8Tfz74XJPqZhkV6luseXpEDIzTeYfy4PIZZW9gV6hAxC4/mGDj', 'crossorigin' => 'anonymous'])
 @basset(base_path('vendor/backpack/theme-tabler/resources/assets/js/tabler.js'))
--- src/AddonServiceProvider.php
+++ src/AddonServiceProvider.php
@@ -19,8 +19,5 @@ class AddonServiceProvider extends ThemeServiceProvider
         $this->app->afterResolving(BladeCompiler::class, function () {
             Blade::componentNamespace('Backpack\\ThemeTabler\\View\\Components', $this->packageName);
         });
-        
-        Basset::map('bp-tabler-css', 'https://cdn.jsdelivr.net/npm/@tabler/core@1.3.2/dist/css/tabler.min.css', ['integrity' => 'sha384-7aLJEogmAYs7X+MSqgKyrK8EQgOyuqTPfYrAs0hJt6IOuVczGdjpiNMmHiXRozR5', 'crossorigin' => 'anonymous']);
-        Basset::map('bp-tabler-js', 'https://cdn.jsdelivr.net/npm/@tabler/core@1.3.2/dist/js/tabler.min.js', ['integrity' => 'sha384-DaKT+KCi9ypTlo8Tfz74XJPqZhkV6luseXpEDIzTeYfy4PIZZW9gV6hAxC4/mGDj', 'crossorigin' => 'anonymous']);
     }
 }
