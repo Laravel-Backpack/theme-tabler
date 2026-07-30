e6bc2a2ad4215d99bf03f62116fc1d89d3bee279
pxpm <pxpm88@gmail.com>
2026-07-30 15:24:29 +0100

    update notifications

--- resources/views/inc/alerts.blade.php
+++ resources/views/inc/alerts.blade.php
@@ -1,26 +1,19 @@
-{{-- Bootstrap Notifications using Prologue Alerts & PNotify JS --}}
+{{-- Bootstrap Notifications using Backpack Alerts --}}
 <script type="text/javascript">
     // This is intentionaly run after dom loads so this way we can avoid showing duplicate alerts
     // when the user is beeing redirected by persistent table, that happens before this event triggers.
     document.onreadystatechange = function() {
         if (document.readyState == "interactive") {
-            Noty.overrideDefaults({
-                layout: 'topRight',
-                theme: 'backstrap',
-                timeout: 2500,
-                closeWith: ['click', 'button'],
-            });
-
             // get alerts from the alert bag
             var $alerts_from_php = JSON.parse('@json(\Alert::getMessages())');
 
             // get the alerts from the localstorage
-            var $alerts_from_localstorage = JSON.parse(localStorage.getItem('backpack_alerts')) ?
-                JSON.parse(localStorage.getItem('backpack_alerts')) : {};
+            var $alerts_from_localstorage = JSON.parse(localStorage.getItem('backpack_alerts'))
+                    ? JSON.parse(localStorage.getItem('backpack_alerts')) : {};
 
             // merge both php alerts and localstorage alerts
             Object.entries($alerts_from_php).forEach(function(type) {
-                if (typeof $alerts_from_localstorage[type[0]] !== 'undefined') {
+                if(typeof $alerts_from_localstorage[type[0]] !== 'undefined') {
                     type[1].forEach(function(msg) {
                         $alerts_from_localstorage[type[0]].push(msg);
                     });
@@ -30,13 +23,13 @@
             });
 
             for (var type in $alerts_from_localstorage) {
-                let messages = new Set($alerts_from_localstorage[type]);
-
-                messages.forEach(function(text) {
-                    let alert = {};
-                    alert['type'] = type;
-                    alert['text'] = text;
-                    new Noty(alert).show()
+                var messages = $alerts_from_localstorage[type];
+                messages.forEach(function(msg) {
+                    if (typeof msg === 'string') {
+                        msg = {message: msg};
+                    }
+                    msg['type'] = type;
+                    new Noty(msg).show();
                 });
             }
 
